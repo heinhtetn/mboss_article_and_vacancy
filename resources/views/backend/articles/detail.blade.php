9c2c256028c9d380c483c03cd6ae192877e04430
myatnoemaung97 <myatnoemaung001@gmail.com>
2023-10-11 15:56:20 +0630

    vacancy crud

---
+++ resources/views/backend/articles/detail.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.master')
+
+@section('article', 'nav-link active')
+
+@section('content')
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Article Details</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <a href="{{route('articles.index')}}" class="btn btn-success float-right">Back</a>
+                    </div>
+
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="card mb-4" style="max-width: 1200px; margin: auto">
+                    <div class="row">
+                        <div class="col-7" style="padding: 20px 0 20px 40px">
+                            <div class="d-flex justify-content-center">
+                                <img style="width: 100%; height: 540px;" src="{{url('featured/' . $article->featured_image)}}" alt="">
+                            </div>                           
+                        </div>
+                        <div class="col-5">
+                            <div class="row p-3">
+                                <div class="col mb-3">
+                                    <div class="d-flex justify-content-center">
+                                        <img style="width: 415px; height: 265px;" src="{{url('uploads/' . $image[0])}}" alt="">
+                                    </div>
+                                    
+                                </div>
+                                <div class="col">
+                                    <div class="d-flex justify-content-center">
+                                        <img style="width: 415px; height: 265px;" src="{{url('uploads/' . $image[1])}}" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3" style="max-width: 1200px; margin: auto">
+                    <div class="row">
+                        <div class="col-6 px-4 pt-2">
+                            <h3>{{$article->title}}</h3> by Admin,  <small class="text-sm" style="font-weight: bold;">{{ $article->created_at->diffForHumans() }}</small>
+                        </div>
+                        <div class="col-6 px-4 pt-2">
+                            <a href="{{ route('articles.edit', $article->id)}}" class="btn btn-success float-right">Edit Article</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="p-3" style="text-align: justify">
+                                {{$article->body}}
+                            </p>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <!-- /.content -->
+    </div>
+
+
+@endsection
---
+++ resources/views/backend/articles/edit.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.master')
+
+@section('article', 'nav-link active')
+
+@section('content')
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Articles Edit Page</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <a href="{{route('articles.index')}}" class="btn btn-success float-right">Back</a>
+                    </div>
+
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <form method="POST" action="{{route('articles.update', $article->id)}}" enctype="multipart/form-data">
+                    @method('PUT')
+                    @csrf
+                    <div class="form-group mb-3">
+                        <label class="form-label" for="title">Title</label>
+                        <input type="text" name="title" class="form-control" value="{{old('title', $article->title)}}" placeholder="Title">
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="body">Description</label>
+                        <textarea class="form-control" name="body" cols="30" rows="10">{{old('body', $article->body)}}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Featured Image</label><br>
+                        <img src="{{url('featured/' . $article->featured_image)}}" style="width: 150px; height: 100px" alt=""><br><br>
+                        <input type="file" name="featured_image">
+                   </div>
+                    <div class="form-group">
+                        <label for="" class="form-label">Content Image</label><br>
+                        <img style="width: 150px; height: 100px;" src="{{url('uploads/' . $image[0])}}" alt="">
+                        <img style="width: 150px; height: 100px;" src="{{url('uploads/' . $image[1])}}" alt="">
+                        <br><br>
+                        <input type="file" name="image[]" multiple accept="image/*" onchange="checkFileCount(this)">
+                   </div>
+
+                    <button type="submit" id="saveButton" class="btn btn-success mt-3">Update Article</button>
+
+                </form>
+
+                <p id="message"></p>
+            </div>
+        </div>
+        <!-- /.content -->
+    </div>
+
+
+@endsection
+
+@section('script')
+    <script>
+
+        function checkFileCount(input) {
+            var maxFiles = 2;
+            var fileCount = input.files.length;
+            var fileCountMessage = document.getElementById('message');
+            var button = document.getElementById('saveButton');
+
+            if (fileCount > maxFiles) {
+
+                fileCountMessage.textContent = 'You can only upload up to ' + maxFiles + ' files.';
+                button.disabled = true; // Disable the submit button
+
+            } else {
+
+                fileCountMessage.textContent = '';
+                button.disabled = false; // Enable the submit button
+
+            }
+        }
+    </script>
+@endsection
+
